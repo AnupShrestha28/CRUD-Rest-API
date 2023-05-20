1cfcf999c0158f6ad64f2d7aa4c5f4d05f4e2e48
AnupShrestha28 <anupstha0168@gmail.com>
2023-05-20 18:12:07 +0545

    Done

--- routes/items.js
+++ routes/items.js
@@ -7,44 +7,44 @@ let data = [
         title: "Create a project",
         order: 1,
         completed: true,
-        createdOn: new Date(),
+        createdOn: new Date()
     },
     {
         id: 2,
-        title: "Take a coffee",
+        title: "Make a coffee",
         order: 2,
         completed: true,
-        createdOn: new Date(),
+        createdOn: new Date()
     },
     {
         id: 3,
-        title: "Write new article",
+        title: "Have some meal",
         order: 3,
         completed: true,
-        createdOn: new Date(),
+        createdOn: new Date()
     },
     {
         id: 4,
         title: "Walk toward home",
         order: 4,
         completed: false,
-        createdOn: new Date(),
+        createdOn: new Date()
     },
     {
         id: 5,
-        title: "Have some dinner",
+        title: "Do some workouts",
         order: 5,
         completed: false,
-        createdOn: new Date(),
+        createdOn: new Date()
     },
 ];
 
-router.get("/", function(req, res){
+router.get("/", (req, res)=>{
     res.status(200).json(data);
 });
 
-router.get("/:id", function(req, res){
-    let found = data.find(function(item){
+router.get("/:id", (req, res)=>{
+    let found = data.find((item)=>{
         return item.id === parseInt(req.params.id);
     });
 
@@ -55,46 +55,43 @@ router.get("/:id", function(req, res){
     }
 });
 
-router.post("/", function(req, res){
+router.post("/", (req, res) => {
     const newItem = {
         id: data.length + 1,
         title: req.body.title,
         order: req.body.order,
         completed: req.body.completed,
-        createdOn: new Date(),
-    };
-
-    data.push(newItem);
-    res.status(201).json(data);
+        createdOn: new Date()
+    }
+        data.push(newItem);
+        res.status(201).json({message: "New data has been added", data: newItem});
 });
 
-
-router.put("/:id", function(req, res){
-    let found = data.find(function(item){
+router.put("/:id", (req, res)=>{
+    let found = data.find((item)=>{
         return item.id === parseInt(req.params.id);
     });
-
     if(found){
-        found.title = req.body.title;
-        found.order = req.body.order;
-        found.completed = req.body.completed;
-        res.sendStatus(204);
+        found.title = req.body.title,
+        found.order = req.body.order,
+        found.completed = req.body.completed,
+        res.status(200).json({message: "Data has been updated", data:found});
     }else{
         res.sendStatus(404);
     }
 });
 
-router.delete("/:id", function(req, res){
-    let index = data.findIndex(function(item){
+router.delete("/:id", (req, res)=>{
+    let index = data.findIndex((item)=>{
         return item.id === parseInt(req.params.id);
     });
 
     if(index !== -1){
         data.splice(index, 1);
-        res.sendStatus(204);
+        res.status(200).json({message: "Data has been deleted"});
     }else{
         res.sendStatus(404);
     }
-});
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const http = require("http");
 const express = require("express");
 var cors = require("cors");
-const itemsRouter = require("./routes/items");
+const itemRouter = require("./routes/items");
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({origin: "http://localhost:8100"}));
+app.use(cors({origin: "http:localhost:8100"}));
 
-app.use("/items", itemsRouter);
+app.use("/items", itemRouter);
 
-app.use("/", function (req,res){
-    res.send("Hello this is speaking from port 3000");
-});
+app.use("/", (req, res)=>{
+    res.send("Hello this is speaking from port 8000");
+})
 
 const server = http.createServer(app);
-const port = 3000;
+const port = 8000;
 server.listen(port);
 
-console.debug("Server listening on port " + port);
+console.debug("Server started at port " + port);
